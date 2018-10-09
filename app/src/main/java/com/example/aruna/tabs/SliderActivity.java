802ae7fd9c00f47a67fb984822f8ed95f40125c6
unknown <admgbit@gmail.com>
2018-10-09 11:56:07 +0530

    Removed Unwanted Classes and Layouts

--- app/src/main/java/com/example/aruna/tabs/SliderActivity.java
+++ app/src/main/java/com/example/aruna/tabs/SliderActivity.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SliderActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener, Tab2.OnFragmentInteractionListener, Tab3.OnFragmentInteractionListener {
+public class SliderActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener {
 
     TabLayout tabLayout;
     List <String> imageURLlist = new ArrayList<>();
